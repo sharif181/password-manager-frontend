2cad1bfbaabcdda3f2e40d2b92d0339ab9266bf8
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-31 10:26:17 +0600

    design login page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div class="relative flex flex-col justify-center h-screen overflow-hidden">
+      <div class="w-full m-auto bg-white rounded-md shadow-md lg:max-w-lg">
+        <h1 class="text-3xl font-semibold text-center text-purple-700">
+          Login
+        </h1>
+        <form class="space-y-4">
+          <div>
+            <label class="label">
+              <span class="text-base label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email Address"
+              class="w-full input input-bordered input-primary"
+            />
+          </div>
+          <div>
+            <label class="label">
+              <span class="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              class="w-full input input-bordered input-primary"
+            />
+          </div>
+          <Link
+            to="/"
+            class="text-xs text-gray-600 hover:underline hover:text-blue-600"
+          >
+            Forget Password?
+          </Link>
+          <div className="flex justify-center pb-2">
+            <button class="btn btn-primary text-bold">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
