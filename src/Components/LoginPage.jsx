2ec14a5e636d455f38357fe9dc70bee7a96a3fe6
Sharif Khan <sharifkhan74647464@gmail.com>
2024-08-01 20:20:56 +0600

    install and setup react hook form

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,41 +1,51 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div class="relative flex flex-col justify-center h-screen overflow-hidden">
-      <div class="w-full m-auto bg-white rounded-md shadow-md lg:max-w-lg">
-        <h1 class="text-3xl font-semibold text-center text-purple-700">
+    <div className="relative flex flex-col justify-center h-screen overflow-hidden">
+      <div className="w-full m-auto bg-white rounded-md shadow-md lg:max-w-lg">
+        <h1 className="text-3xl font-semibold text-center text-purple-700">
           Login
         </h1>
-        <form class="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <label class="label">
-              <span class="text-base label-text">Email</span>
+            <label className="label">
+              <span className="text-base label-text">Email</span>
             </label>
             <input
+              {...register("email")}
               type="text"
               placeholder="Email Address"
-              class="w-full input input-bordered input-primary"
+              className="w-full input input-bordered input-primary"
             />
           </div>
           <div>
-            <label class="label">
-              <span class="text-base label-text">Password</span>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
             </label>
             <input
+              {...register("password")}
               type="password"
               placeholder="Enter Password"
-              class="w-full input input-bordered input-primary"
+              className="w-full input input-bordered input-primary"
             />
           </div>
           <Link
             to="/"
-            class="text-xs text-gray-600 hover:underline hover:text-blue-600"
+            className="text-xs text-gray-600 hover:underline hover:text-blue-600"
           >
             Forget Password?
           </Link>
           <div className="flex justify-center pb-2">
-            <button class="btn btn-primary text-bold">Login</button>
+            <button type="submit" className="btn btn-primary text-bold">
+              Login
+            </button>
           </div>
         </form>
       </div>
