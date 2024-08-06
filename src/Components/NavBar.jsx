7f777b3a3bf65320afa85107e092320c71ed0a91
Sharif Khan <sharifkhan74647464@gmail.com>
2024-08-06 21:06:22 +0600

    show login button based on login status

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const NavBar = () => {
+  const { isLoggin } = useSelector((state) => state.auth);
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -8,34 +11,39 @@ const NavBar = () => {
         </Link>
       </div>
       <div className="flex-none">
-        <Link to="/login" className="link link-primary">
-          Login
-        </Link>
-        {/* <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="profile"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+        {!isLoggin && (
+          <Link to="/login" className="link link-primary">
+            Login
+          </Link>
+        )}
+
+        {isLoggin && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="profile"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">Profile</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div> */}
+        )}
       </div>
     </div>
   );
